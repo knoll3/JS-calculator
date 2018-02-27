3d13b2383d6d882dca09908767694977f83a8001
knoll3 <noll.kyle@gmail.com>
2018-02-27 20:02:25 +0000

    Add button labels

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ function operate(op, n1, n2) {
   return eval(`${n1} ${op} ${n2}`);
 }
 
-document.getElementById('container').innerHTML = operate('+', 5, 3);
+var buttonLabels = ['+-', 'C', '<-', '/',
+                    '7',  '8',  '9', 'X', 
+                    '4',  '5',  '6', '-',
+                    '1',  '2',  '3', '+',
+                    '',   '0',  '.', '='];
+
+for (let i = 0; i < 20; i++ ) {
+  let button = document.createElement('button');
+  button.setAttribute('class', 'button');
+  button.innerHTML = buttonLabels[i];
+  document.getElementById('container').appendChild(button);
+}
