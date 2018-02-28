62f1f191eab2855b671d076a6fcc5d5b643ecdec
Knoll <noll.kyle@gmail.com>
2018-02-27 20:42:16 -0600

    JS completely working

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function genButtonLabels() {
 function makeCalculator() {
   var buttonLabels = genButtonLabels();
   result = '';
+  resultSave = '';
   for (let i = 0; i < 20; i++ ) {
     let button = document.createElement('button');
     button.setAttribute('class', 'button');
@@ -34,10 +35,20 @@ function onButtonPress(button) {
   if (button == 'C') {
     result = '';
   } else if (button == '=') {
-    result = eval(result);
+    result = eval(result)
+    resultSave = result;
+    document.getElementById('bottomDisplay').innerHTML = result;
+    result = '';
+    return;
   } else if (button == '<-') {
     result = String(result).substring(0, result.length - 1);
   } else {
+    if (resultSave !== '' && (button == '+' || button == '-' || button == '*' || button == '/')) {
+      result = resultSave;
+      resultSave = '';
+    } else {
+      resultSave = '';
+    }
     result += button;
   }
   console.log(result);
